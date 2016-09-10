0390e3c7e704bbe482e1e1fea460421215546d48
lisong <song.li@witontek.com>
2016-09-10 11:35:58 +0800

    init

---
+++ lswt/src/main/java/ls/demon/xx/web/controller/T2Controller.java
@@ -0,0 +1,123 @@
+/**
+ * Witontek.com.
+ * Copyright (c) 2012-2016 All Rights Reserved.
+ */
+package ls.demon.xx.web.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import ls.demon.xx.web.controller.form.XXForm;
+
+/**
+ * 
+ * http://localhost:8080/lswt/t2/6.j
+ * http://localhost:8080/lswt/t2/5.j
+ * 
+ * http://localhost:8080/lswt/t2/4.j not ok
+ * http://localhost:8080/lswt/t2/3.j
+ * 
+ * http://localhost:8080/lswt/t2/2.j
+ * http://localhost:8080/lswt/t2/1.j
+ * 
+ * http://localhost:8080/lswt/tool/at.htm
+ * 
+ * http://localhost:8080/lswt/ls/index.jsp
+ * 
+ * @author lisong
+ * @version $Id: T2Controller.java, v 0.1 2016年9月10日 上午10:58:03 lisong Exp $
+ */
+@Controller
+public class T2Controller {
+    /**
+    * Logger for this class
+    */
+    private static final Logger logger = LoggerFactory.getLogger(T2Controller.class);
+
+    @RequestMapping(value = "/t2/1.j", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String t1(ModelMap modelMap, String appId, String appSecret) {
+        logger.info("appId={},appSecret={}", appId, appSecret);
+
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("1", "abc");
+        map.put("2", "中文");
+
+        modelMap.addAttribute("map", map);
+        return "json.vm";
+    }
+
+    @RequestMapping(value = "/t2/2.j", produces = MediaType.APPLICATION_JSON_VALUE)
+    public void t2(ModelMap modelMap, String appId, String appSecret) {
+        logger.info("appId={},appSecret={}", appId, appSecret);
+
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("1", "abc");
+        map.put("2", "中文");
+
+        modelMap.addAttribute("map", map);
+        return;
+    }
+
+    @RequestMapping(value = "/t2/3.j", produces = MediaType.APPLICATION_JSON_VALUE
+                                                  + ";charset=UTF-8")
+    @ResponseBody
+    public String t3(ModelMap modelMap, String appId, String appSecret) {
+        logger.info("appId={},appSecret={}", appId, appSecret);
+
+        JSONObject jo = new JSONObject();
+        jo.put("a", "b");
+        jo.put("c", "中文");
+
+        return jo.toString();
+    }
+
+    @RequestMapping(value = "/t2/4.j", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public JSONObject t4(ModelMap modelMap, String appId, String appSecret) {
+        logger.info("appId={},appSecret={}", appId, appSecret);
+
+        JSONObject jo = new JSONObject();
+        jo.put("a", "b");
+        jo.put("c", "中文");
+
+        return jo;
+    }
+
+    @RequestMapping(value = "/t2/5.j", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public XXForm json3(ModelMap modelMap, String appId, String appSecret) throws JSONException {
+        logger.info("appId={},appSecret={}", appId, appSecret);
+        modelMap.addAttribute("1", "abc");
+        modelMap.addAttribute("2", "中文");
+
+        XXForm r = new XXForm();
+        r.setAppId("aa");
+        r.setTest中文("中外那");
+
+        return r;
+    }
+
+    @RequestMapping(value = "/t2/6.j", produces = MediaType.APPLICATION_XML_VALUE)
+    @ResponseBody
+    public XXForm xml1(ModelMap modelMap, String appId, String appSecret) {
+        logger.info("appId={},appSecret={}", appId, appSecret);
+        modelMap.addAttribute("1", "abc");
+        modelMap.addAttribute("2", "中文");
+
+        XXForm r = new XXForm();
+        r.setAppId("aa");
+        r.setTest中文("中外那");
+
+        return r;
+    }
+}
---
+++ lswt/src/main/java/ls/demon/xx/web/controller/TestController.java
@@ -0,0 +1,132 @@
+/**
+ * Witontek.com.
+ * Copyright (c) 2012-2016 All Rights Reserved.
+ */
+package ls.demon.xx.web.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import ls.demon.xx.web.controller.form.XXForm;
+
+/**
+ * 
+ * http://localhost:8080/lswt/test/show.htm
+ * http://localhost:8080/lswt/test/showJson.json
+ * http://localhost:8080/lswt/test/showJson.htm
+ * 
+ * http://localhost:8080/lswt/test/json1.json not_ok
+ * http://localhost:8080/lswt/test/json2.json
+ * 
+ * http://localhost:8080/lswt/test/json3.json
+ * 
+ * http://localhost:8080/lswt//test/xml1.xml
+ * 
+ * 
+ * @author lisong
+ * @version $Id: TestController.java, v 0.1 2016年9月9日 下午7:51:41 lisong Exp $
+ */
+@Controller
+public class TestController {
+    /**
+    * Logger for this class
+    */
+    private static final Logger logger = LoggerFactory.getLogger(TestController.class);
+
+    @RequestMapping(value = "/test/show.htm")
+    public String doGet(ModelMap modelMap, String appId, String appSecret) {
+        logger.info("appId={},appSecret={}", appId, appSecret);
+        modelMap.addAttribute("name", "hello 你好");
+        return "test.vm";
+    }
+
+    @RequestMapping(value = "/test/showJson.htm")
+    public String showJson(ModelMap modelMap, String appId, String appSecret) {
+        logger.info("appId={},appSecret={}", appId, appSecret);
+
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("1", "abc");
+        map.put("2", "中文");
+
+        modelMap.addAttribute("map", map);
+        return "json.vm";
+    }
+
+    @RequestMapping(value = "/test/showJson.json")
+    public void showJson2(ModelMap modelMap, String appId, String appSecret) {
+        logger.info("appId={},appSecret={}", appId, appSecret);
+
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("1", "abc");
+        map.put("2", "中文");
+
+        modelMap.addAttribute("map", map);
+        return;
+    }
+
+    @RequestMapping(value = "/test/json1.json")
+    @ResponseBody
+    public JSONObject json1(ModelMap modelMap, String appId,
+                            String appSecret) throws JSONException {
+        logger.info("appId={},appSecret={}", appId, appSecret);
+        modelMap.addAttribute("1", "abc");
+        modelMap.addAttribute("2", "中文");
+
+        JSONObject jo = new JSONObject();
+        jo.put("a", "b");
+        jo.put("c", "中文");
+
+        return jo;
+    }
+
+    @RequestMapping(value = "/test/json2.json")
+    @ResponseBody
+    public String json2(ModelMap modelMap, String appId, String appSecret) throws JSONException {
+        logger.info("appId={},appSecret={}", appId, appSecret);
+        modelMap.addAttribute("1", "abc");
+        modelMap.addAttribute("2", "中文");
+
+        JSONObject jo = new JSONObject();
+        jo.put("a", "b");
+        jo.put("c", "中文");
+
+        return jo.toString();
+    }
+
+    @RequestMapping(value = "/test/json3.json")
+    @ResponseBody
+    public XXForm json3(ModelMap modelMap, String appId, String appSecret) throws JSONException {
+        logger.info("appId={},appSecret={}", appId, appSecret);
+        modelMap.addAttribute("1", "abc");
+        modelMap.addAttribute("2", "中文");
+
+        XXForm r = new XXForm();
+        r.setAppId("aa");
+        r.setTest中文("中外那");
+
+        return r;
+    }
+
+    @RequestMapping(value = "/test/xml1.xml")
+    @ResponseBody
+    public XXForm xml1(ModelMap modelMap, String appId, String appSecret) {
+        logger.info("appId={},appSecret={}", appId, appSecret);
+        modelMap.addAttribute("1", "abc");
+        modelMap.addAttribute("2", "中文");
+
+        XXForm r = new XXForm();
+        r.setAppId("aa");
+        r.setTest中文("中外那");
+
+        return r;
+    }
+}
